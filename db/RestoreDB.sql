45068844ff997d4f87534bdfb643f3b1ecb23fe4
Roman <91251223+djsra1@users.noreply.github.com>
2021-12-07 20:02:15 +0300

    Update RestoreDB.sql

--- db/RestoreDB.sql
+++ db/RestoreDB.sql
@@ -2,4 +2,4 @@ USE master
 go
 select 'Restoring DB from backup'
 go
-RESTORE DATABASE [stego_as] FROM  DISK = N'/var/opt/mssql/data/stego_as.bak' WITH  FILE = 1,  MOVE N'stego_as' TO N'/var/opt/mssql/data/stego_as.mdf',  MOVE N'stego_as_log' TO N'/var/opt/mssql/data/stego_as_log.ldf',  NOUNLOAD,  REPLACE,  STATS = 5
+RESTORE DATABASE [YOUR_DB_NAME] FROM  DISK = N'/var/opt/mssql/data/YOUR_BACKUP_NAME.bak' WITH  FILE = 1,  MOVE N'YOUR_MDF_FILE_NAME' TO N'/var/opt/mssql/data/YOUR_MDF_FILE_NAME.mdf',  MOVE N'YOUR_LDF_FILE_NAME' TO N'/var/opt/mssql/data/YOUR_LDF_FILE_NAME.ldf',  NOUNLOAD,  REPLACE,  STATS = 5
